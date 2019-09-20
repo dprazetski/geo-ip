4c1e64b84239e5b10b1480b28fbc4750609a23d0
Roman Bondarenko <roman.bondarenko@idt.net>
2019-09-20 15:59:41 +0300

    BRS-13609 Geo_IP loading script and the whole process

--- bin/load_to_db.sh
+++ bin/load_to_db.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 ########################################
 ##### variables initialization #########
-. $HOME/.profile > /dev/null
 dumpDate=`date +%Y%m%d%H%M%S`
 main_GeoIP_DIR=/u/debit/geo_ip
 SqlLdr_DIR=$main_GeoIP_DIR/Oracle_Loader
@@ -9,7 +8,6 @@ SqlLdr_DIR=$main_GeoIP_DIR/Oracle_Loader
 ########## DB Configuration ############
 DBUser=geoip
 DBPass=geoip
-DBHost=ex01-scan.prod.idt.net
 DBServiceType=RETAIL
 ########################################
 ############    MAIN    ################
@@ -17,21 +15,11 @@ echo "##################################"
 date '+Started           : %Y.%m.%d %T'
 echo "ProcessedFile     :       $SqlLdr_DIR/ipdata.out"
 cd $SqlLdr_DIR
-echo "Dir changed to `pwd`"
-echo "##################################"
 echo "Loading to DB ..."
-# (sqlplus -S -L ${DBUser}/${DBPass}@//${DBHost}/${DBServiceType} @${dirToCTL}/truncate_CRE_BALANCE_DUMP3.sql)
-# (sqlplus -S -L ${DBUser}/${DBPass}@//${DBHost}/${DBServiceType} @${dirToCTL}/analize_CRE_BALANCE.sql)
-for i in $SqlLdr_DIR/ipdata.out
-do
-  csvName=`basename ${i%.*}`
-  name=${csvName%_*}
-  echo "Start with File   :       $csvName"
-  (sqlldr userid=${DBUser}/${DBPass}@//${DBHost}/${DBServiceType} control=$SqlLdr_DIR/load.ctl data=${i} log=$SqlLdr_DIR/load.log bad=$SqlLdr_DIR/load.bad discard=$SqlLdr_DIR/load.dsc) | tail -1
-done
+(sqlldr userid=$DBUser/$DBPass@$DBServiceType control=$main_GeoIP_DIR/bin/load_to_db.ctl data=ipdata.out log=load.log bad=load.bad discard=load.dsc) | tail -1
 sleep 1
-echo "##################################"
 date '+Loader ended      : %Y.%m.%d %T'
+echo "##################################"
 #echo "Analize tables ..."
 #(sqlplus -S -L ${DBUser}/${DBPass}@//${DBHost}/${DBServiceType} @${dirToCTL}/analize_CRE_BALANCE.sql)
 #date '+Analized          : %Y.%m.%d %T'
